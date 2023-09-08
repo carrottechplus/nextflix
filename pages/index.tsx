c452eff06de8d2bd5ad4e48ef68374a042ce0a3b
carrottechplus <blohzzang@gmail.com>
2023-09-08 10:58:54 +0900

    main 작업

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-	return <main>HELLO NEXTFLIX</main>;
-}
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+	return (
+		<>
+			<Head>
+				<title>NEXTFLIX</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<main>HELLO</main>
+		</>
+	);
+};
+
+export default Home;
