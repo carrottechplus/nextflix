99b79c24c99e2951fec5690a3a69c84c2e9b0cb5
carrottechplus <blohzzang@gmail.com>
2023-09-08 12:12:05 +0900

    header 스타일링 및 스크롤시 배경색 변경 작업

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,40 @@
-// 대입형 함수x > 페이지 컴포넌트만
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { FaBell, FaSearch } from 'react-icons/fa';
+
 function Header() {
+	const [Scrolled, setScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => (window.scrollY > 0 ? setScrolled(true) : setScrolled(false));
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
+
 	return (
-		<header>
-			<h1>NEXTFLIX</h1>
-			<nav>
-				<ul>
-					<li className='headerLink'>Home</li>
-					<li className='headerLink'>TV show</li>
-					<li className='headerLink'>Movies</li>
-					<li className='headerLink'>New & Popular</li>
-					<li className='headerLink'>My list</li>
-				</ul>
-			</nav>
+		<header className={`${Scrolled && 'bg-red-500'}`}>
+			<div className='flex items-center space-x-2 md:space-x-10'>
+				<h1>
+					<img src='https://rb.gy/ulxxee' alt='nextflix' width={100} height={100} className='cursor-pointer' />
+				</h1>
+			</div>
+
+			<ul className='space-x-4 hidden md:flex '>
+				<li className='headerLink'>Home</li>
+				<li className='headerLink'>TV show</li>
+				<li className='headerLink'>Movies</li>
+				<li className='headerLink'>New & Popular</li>
+				<li className='headerLink'>My list</li>
+			</ul>
+
+			<div className='flex items-center space-x-4 text-sm font-light'>
+				<FaSearch className='w-6' />
+				<p className='hidden lg:inline'>KIDS</p>
+				<FaBell className='w-6' />
+				<Link href='/'>
+					<img src='https://rb.gy/g1pwyx' alt='nextflix' width={32} height={32} className='rounded' />
+				</Link>
+			</div>
 		</header>
 	);
 }
