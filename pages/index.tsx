19a11e582b9773027c8076d8110daee1c2effa5f
carrottechplus <blohzzang@gmail.com>
2023-09-11 10:33:56 +0900

    original props 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import { Movie } from '@/types';
 
 interface Props {
+	original: Movie[];
 	top: Movie[];
 	sf: Movie[];
 	animation: Movie[];
@@ -15,8 +16,8 @@ interface Props {
 }
 
 // Next에서 기본으로 제공하는 NextPage 타입에는 커스텀 Props 타입이 설정되어있지 않기 떄문에 Generic을 활용해서 Props 타입의 interface를 직접 변수로 호출할 때 설정
-const Home: NextPage<Props> = ({ top, sf, animation, drama, comedy, fantasy }: Props) => {
-	console.log(fantasy);
+const Home: NextPage<Props> = ({ original, top, sf, animation, drama, comedy, fantasy }: Props) => {
+	console.log(original);
 	return (
 		<div className='relative h-screen bg-gradient-to-b from-[#333] to-[#141414]'>
 			<Head>
@@ -25,9 +26,7 @@ const Home: NextPage<Props> = ({ top, sf, animation, drama, comedy, fantasy }: P
 			</Head>
 
 			<Header />
-			<main>
-				<Image src={`https://image.tmdb.org/t/p/original/${top[0].backdrop_path}`} alt='image' width={500} height={500} />
-			</main>
+			<main>{/* <Image src={`https://image.tmdb.org/t/p/original/${top[0].backdrop_path}`} alt='image' width={500} height={500} /> */}</main>
 		</div>
 	);
 };
@@ -35,7 +34,8 @@ const Home: NextPage<Props> = ({ top, sf, animation, drama, comedy, fantasy }: P
 export default Home;
 
 export const getServerSideProps = async () => {
-	const [top, sf, animation, drama, comedy, fantasy] = await Promise.all([
+	const [original, top, sf, animation, drama, comedy, fantasy] = await Promise.all([
+		fetch(requests.original).then((res) => res.json()),
 		fetch(requests.top).then((res) => res.json()),
 		fetch(requests.sf).then((res) => res.json()),
 		fetch(requests.animation).then((res) => res.json()),
@@ -45,6 +45,6 @@ export const getServerSideProps = async () => {
 	]);
 
 	return {
-		props: { top: top.results, sf: sf.results, animation: animation.results, drama: drama.results, comedy: comedy.results, fantasy: fantasy.results },
+		props: { original: original.results, top: top.results, sf: sf.results, animation: animation.results, drama: drama.results, comedy: comedy.results, fantasy: fantasy.results },
 	};
 };
