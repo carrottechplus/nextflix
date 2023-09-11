4ee1580f8d178774d782a24cfc5f3fae42304252
carrottechplus <blohzzang@gmail.com>
2023-09-11 10:34:04 +0900

    banner 컴포넌트 추가

---
+++ components/Banner.tsx
@@ -0,0 +1,15 @@
+import { Movie } from '@/types';
+import { useEffect, useState } from 'react';
+
+interface Props {
+	original: Movie[];
+}
+
+function Banner({ original }: Props) {
+	const [Movie, setMovie] = useState<Movie | null>(null);
+	// 함수에 타입지정이 필수이지만 내장 hook 이기 떄문에 타입 안써도 되긴하나 오류방지 차원에서 generic이용하여 작성
+	useEffect(() => {}, []);
+	return <div>Banner</div>;
+}
+
+export default Banner;
