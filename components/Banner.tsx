217714352501eee43750e9a63a34f8878c17a503
carrottechplus <blohzzang@gmail.com>
2023-09-11 11:18:30 +0900

    banner 컴포넌트 스타일링 및 영화 정보 출력

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,4 +1,6 @@
 import { Movie } from '@/types';
+import { baseURL } from '@/url';
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 interface Props {
@@ -10,9 +12,21 @@ function Banner({ original }: Props) {
 	// 함수에 타입지정이 필수이지만 내장 hook 이기 떄문에 타입 안써도 되긴하나 오류방지 차원에서 generic이용하여 작성
 	useEffect(() => {
 		const randomNum = Math.floor(Math.random() * original.length); // 정수로 반환
+		console.log(randomNum);
 		setMovie(original[randomNum]);
+		console.log(setMovie);
 	}, [original]);
-	return <div>Banner</div>;
+	return (
+		<section className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
+			<div className='absolute top-0 left-0 z-[1] h-[95vh] w-full'>
+				<Image src={`${baseURL}${Movie?.backdrop_path}`} alt={`${Movie?.title || Movie?.name}`} fill priority quality={50} className='object-cover' />
+			</div>
+
+			{/* title */}
+			<h2 className='relative z-[2]'>{Movie?.title || Movie?.name}</h2>
+			<p className='relative z-[2]'>{Movie?.overview}</p>
+		</section>
+	);
 }
 
 export default Banner;
