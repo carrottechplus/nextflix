acebe1b4322ec623cc1eb39aeabf1c17fc1490b8
carrottechplus <blohzzang@gmail.com>
2023-09-14 15:41:50 +0900

    submit이벤트 핸들러 함수 연결

--- pages/login.tsx
+++ pages/login.tsx
@@ -19,6 +19,13 @@ function Login() {
 	} = useForm<Inputs>();
 	// 객체 반환하는데 객체안에 위 reg, hand, form 반환함, 근데 formState 안에 또 객체가 존재 (중첩 객체)
 
+	//handleSubmit 함수 인증처리 완료시 동기적으로 실행될 콜백함수 등록
+	//해당 콜백함수는 인증에 성공했을만 호출: 인수로 전달되는 값은 관리되고 있는 form의 value값
+	const join: SubmitHandler<Inputs> = async ({ email, password }) => {
+		console.log('email', email);
+		console.log('password', password);
+	};
+
 	return (
 		<main className='relative flex w-full h-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent'>
 			<Head>
@@ -29,11 +36,16 @@ function Login() {
 
 			<Image width={150} height={150} src={logo} alt='logo' className='absolute left-4 top-4 cursor-pointer md:left-10 md:top-6' sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw' />
 
-			<form className='relative mt-24 space-y-8 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-md md:px-14'>
+			<form
+				onSubmit={handleSubmit(join)} //submit이벤트가 발생시 handleSubmit이 인증처리를 해주고 인증의 결과값을 등록된 콜백함수에 전달
+				className='relative z-50 mt-24 space-y-8 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-md md:px-14'
+			>
 				<h2 className='text-4l font-semibold'>Sign In</h2>
 				<div className='space-y-4'>
-					<input type='email' placeholder='Email' className='input' id='' />
-					<input type='password' placeholder='Password' className='input' id='' />
+					<input type='email' placeholder='Email' className='input' {...register('email', { required: true })} />
+					{errors.email && <span>Please enter a valid Email</span>}
+					<input type='password' placeholder='Password' className='input' {...register('password', { required: true })} />
+					{errors.password && <span>Please enter a valid Password</span>}
 				</div>
 
 				<button type='button' className='w-full rounded bg-[#e40914] py-3 font-semibold'>
